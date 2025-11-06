e8a49e36c76e9862abe12ebd60eeb1ebb577c95f
Xavier <rafael.xavier.vital@gmail.com>
2025-11-07 00:15:58 +0100

    math_operator file and db-administrator.py removed

--- db-administrator.py
+++
@@ -1,2 +0,0 @@
-#This file is going to have the administrator for the databank
-
--- operator.py
+++
@@ -1,1 +0,0 @@
-#This file is going to have all functions used to have all functions used to make the math operations
