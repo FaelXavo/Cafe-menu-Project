f0ca0025f2f376bc68fcc5fe1cfbf53234924f8f
Xavier <rafael.xavier.vital@gmail.com>
2025-11-06 23:25:04 +0100

    Tables for items, order, history and statistics created and working

---
+++ database.py
@@ -0,0 +1,53 @@
+import _sqlite3
+from db_administrator import *
+
+# items: id, name, price, category
+# orders: id, item_id, quantity, total_value
+# history: id, date, items, total
+# statistics: date, daily_total, monthly_total
+
+conn = _sqlite3.connect('cafe_data.db')
+
+c = conn.cursor()
+c.execute('''
+    CREATE TABLE IF NOT EXISTS items (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        name TEXT NOT NULL,
+        price INTEGER NOT NULL,
+        category TEXT
+    )
+''')
+c.execute('''
+    CREATE TABLE IF NOT EXISTS orders (
+        id INTEGER PRIMARY KEY NOT NULL,
+        item_id INT NOT NULL,
+        quantity INTEGER,
+        date TEXT,
+        total_value INTEGER
+    )
+''')
+c.execute('''
+    CREATE TABLE IF NOT EXISTS orders_history (
+    id INTEGER PRIMARY AUTOINCREMENT KEY NOT NULL,
+    date TEXT,
+    items TEXT,
+    total INTEGER
+    )
+''')
+c.execute('''
+    CREATE TABLE IF NOT EXISTS statistics (
+    id INTEGER PRIMARY KEY NOT NULL,
+    quantity INTEGER,
+    total INTEGER,
+    month_total INTEGER
+    )
+''')
+
+#testing area --------------------------
+name = 'smth'
+price = 1200
+category = 'drink'
+
+#insert_item(name, price, category)
+#edit_price(new_price = 10000, name = name)
+#delete_item(name)
