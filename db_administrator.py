da48e12897c4face613bee26db6d41d1a4a56a03
Xavier <rafael.xavier.vital@gmail.com>
2025-11-08 16:40:41 +0100

    start_order() and add_order_item() functions created to make it possible to work with multiple tables. finish_order() redone. def_open_orders() function created to return all opened tables

--- db_administrator.py
+++ db_administrator.py
@@ -25,10 +25,17 @@ def delete_item(name):
 def calculate_amount(item, quantity):
     return int(item * quantity)
 
+def start_order(table_number):
+    now = datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    with conn:
+        c.execute("INSERT INTO orders (date, table_number) VALUES (?, ?)", (now, table_number))
+        return c.lastrowid
+
 #FUNCTIONS TO MANIPULATE ORDERS
-def new_order(item_id, quantity):
+def add_order_item(item_id, quantity):
     if quantity <= 0:
         raise ValueError("The amount of items must be positive")
+
     c.execute("SELECT name, price FROM items WHERE id = ?", (item_id,))
     row = c.fetchone()
     if row is None:
@@ -37,17 +44,30 @@ def new_order(item_id, quantity):
     name, price = row
     total = calculate_amount(price, quantity)
     with conn:
-        c.execute("INSERT INTO orders (item_name, quantity, total_value) "
+        c.execute("INSERT INTO order_items (item_name, quantity, total_value) "
                   "VALUES (?, ?, ?)",
                   (name, quantity, total))
 
+def finish_order(order_id):
+    now = datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    c.execute("""
+        SELECT group_concat(item_name, ', '), sum(total_value)
+        FROM order_items
+        WHERE order_id = ?
+    """, (order_id,))
+    row = c.fetchone()
+    if not row:
+        raise ValueError("Order doesn't exist")
+    items, total = row
 
-def finish_order():
-    try:
-        with conn:
-            c.execute('''INSERT INTO orders_history (date, items, total)
-                      SELECT ?, group_concat(item_name), sum(total_value) FROM orders''',
-                      (datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S'),))
-            c.execute ('DELETE FROM orders')
-    except _sqlite3.Error as e:
-        print(f"Error to finish the purchase: {e}")
+    with conn:
+        c.execute("""
+                    INSERT INTO orders_history (date, items, total, table_number)
+                    SELECT ?, ?, ?, table_number FROM orders WHERE id = ?
+                """, (now, items, total, order_id))
+        c.execute("UPDATE orders SET status = 'closed' WHERE id = ?", (order_id,))
+        c.execute("DELETE FROM order_items WHERE order_id = ?", (order_id,))
+
+def get_open_orders():
+    c.execute("SELECT id, table_number, date FROM orders WHERE status = 'open'")
+    return c.fetchall()
